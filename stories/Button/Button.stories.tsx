9a1be4b3250756224128343e859d625a094dcc86
Topi Ilmoni <topi.ilmoni@gmail.com>
2024-01-28 02:32:19 +0200

    Changed clsx to tv

--- stories/Button/Button.stories.tsx
+++ stories/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import Button from "./Button.tsx";
 import {Meta} from "@storybook/blocks";
+import {StoryObj} from "@storybook/react";
 
 const meta: Meta<typeof Button> = {
   component: Button,
@@ -16,20 +17,33 @@ type Story = StoryObj<typeof Button>;
 export const Default: Story = {
   args: {
     children: 'Button',
-    variant: 'primary'
+    color: 'primary'
   }
 }
 
-export const Variants = {
+export const Variants: Story = {
   render: (args) => {
     return (
       <div className="grid grid-cols-3 gap-4">
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="success">Success</Button>
-        <Button variant="warning">Warning</Button>
-        <Button variant="error">Error</Button>
-        <Button variant="primary" isDisabled={true}>Disabled</Button>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="success">Success</Button>
+        <Button color="warning">Warning</Button>
+        <Button color="error">Error</Button>
+      </div>
+    )
+  }
+}
+
+export const Disabled: Story = {
+  render: (args) => {
+    return (
+      <div className="grid grid-cols-3 gap-4">
+        <Button color="primary" isDisabled={true}>Primary</Button>
+        <Button color="secondary" isDisabled={true}>Secondary</Button>
+        <Button color="success" isDisabled={true}>Success</Button>
+        <Button color="warning" isDisabled={true}>Warning</Button>
+        <Button color="error" isDisabled={true}>Error</Button>
       </div>
     )
   }
--- stories/Button/Button.tsx
+++ stories/Button/Button.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import {Button as ReactAriaButton} from "react-aria-components";
-import clsx from "clsx";
+import { tv } from 'tailwind-variants';
 
-export default function Button({children, variant, isDisabled, props}) {
-  const tailwindStyles = clsx(
-    "rounded py-2 px-4 font-bold capitalize",
-    {
-      "bg-blue-800 text-white hover:bg-blue-900 focus:outline-blue-900 hover:shadow-md": variant === 'primary' && !isDisabled,
-      "bg-white text-blue-800 border-solid border-2 border-blue-800 hover:bg-gray-50 focus:outline-blue-900 focus:outline-double hover:shadow-md": variant === 'secondary' && !isDisabled,
-      "text-green-800 border-solid border-2 border-green-800 hover:bg-gray-50 hover:shadow-md focus:outline-green-900 focus:outline-double": variant === 'success' && !isDisabled,
-      "text-yellow-600 border-solid border-2 border-yellow-600 hover:bg-gray-50 hover:shadow-md focus:outline-yellow-700 focus:outline-double": variant === 'warning' && !isDisabled,
-      "text-red-800 border-solid border-2 border-red-800 hover:bg-gray-50 hover:shadow-md focus:outline-red-900 focus:outline-double": variant === 'error' && !isDisabled,
-      "bg-slate-100 text-slate-400 border-solid border-2 border-slate-400 outline-none cursor-not-allowed": isDisabled === true
+const button = tv({
+  base: "font-bold capitalize py-2 px-4 rounded hover:shadow-md active:opacity-90",
+  variants: {
+    color: {
+      primary: "bg-blue-600 text-white hover:bg-blue-800 focus:outline-blue-900",
+      secondary: "bg-white text-blue-800 border-solid border-2 border-blue-800 hover:bg-gray-50 focus:outline-blue-900 focus:outline active:bg-blue-50",
+      success: "bg-green-600 text-white hover:bg-green-800 focus:outline-green-900",
+      warning: "bg-yellow-500 text-white hover:bg-yellow-700 focus:outline-yellow-800",
+      error: "bg-red-600 text-white hover:bg-red-800 focus:outline-red-900",
+    },
+    disabled: {
+      true: "bg-gray-100 text-gray-500 opacity-50 border-solid border-2 border-gray-400 cursor-not-allowed hover:bg-gray-100 hover:shadow-none focus:outline-0 active:opacity-50"
     }
-  );
+  }
+})
+
+export default function Button({children, color, isDisabled, props}) {
   return (
     <ReactAriaButton
-      className={tailwindStyles}
+      className={button({ color, disabled: isDisabled })}
       {...props}
     >
       {children}
